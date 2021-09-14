c4d5c12ad7bb42b1b18525ffb23be0e21d370ebd
imjae <code@kakao.com>
2021-09-15 06:41:24 +0900

    캐릭터 움직임 수정(AddForce -> Velocity)

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
+    private Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
     public float speed = 8f; // 이동 속력
 
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.UpArrow))
+        /*if(Input.GetKey(KeyCode.UpArrow))
         {
             // 위쪽 방향키 입력이 감지된 경우 z 방향 힘 주기
             playerRigidbody.AddForce(0f, 0f, speed);
@@ -36,6 +36,25 @@ public class PlayerController : MonoBehaviour
         {
             // 왼쪽 방향키 입력이 감지된 경우 z 방향 힘 주기
             playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
+        }*/
+
+        //수평축과 수직축의 입력값읠 감지하여 저장
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        // 실제 이동속도를 입력값과 이동속력을 사용해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        //Vector3 속도를 (xSpeed, 0 ,zSpeed)로 생성
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+
+        //리지드바디의 속도에 newVelocity 할당
+        playerRigidbody.velocity = newVelocity;
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
