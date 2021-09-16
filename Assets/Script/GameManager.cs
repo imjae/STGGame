a50349c3692fcbabeafb74adf054b9a4589f8e4a
imjae <code@kakao.com>
2021-09-16 21:14:35 +0900

    시간, 최고점수 추가

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI; // UI 관련 라이브러리
+using UnityEngine.SceneManagement; // 씬 관리 관련 라이브러리
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameoverText;
+    public Text timeText;
+    public Text recordText;
+
+    private float surviveTime;
+    private bool isGameover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 생존 시간과 게임오버 상태 초기화
+        surviveTime = 0;
+        isGameover = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isGameover)
+        {
+            // 생존 시간 갱신
+            surviveTime += Time.deltaTime;
+            // 갱신한 생존 시간을 timeText 텍스트 컴포넌트를 이용해 표시
+            timeText.text = $"Timd : {(int)surviveTime}";
+        }else
+        {
+            // 게임오버 상태에서 R 키를 누른경우
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                // SampleScene 씬을 로드
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    // 현재 게임을 게임오버 상태로 변경하는 메서드
+    public void EndGame()
+    {
+        // 현재 상태를 게임오버 상태로 전환
+        isGameover = true;
+        // 게임오버 텍스트 게임 오브젝트를 활성화
+        gameoverText.SetActive(true);
+
+        // BestTime 키로 저장된 이전까지의 최고 기록 가져오기
+        float bestTime = PlayerPrefs.GetFloat("BsetTime", 0f);
+
+        // 이전까지의 최고 기록보다 현재 생존시간이 더 클 경우
+        if(surviveTime > bestTime)
+        {
+            // 최고 기록 값을 현재 생존 시간 값으로 변경
+            bestTime = surviveTime;
+            // 변경된 최고 기록을 BestTiem 키로 저장
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+        }
+
+        // 최고 기록을 recordText 텍스트 컴포넌트를 이용해 표시
+        recordText.text = $"Best Time : {(int)bestTime}";
+            
+    }
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -56,5 +56,10 @@ public class PlayerController : MonoBehaviour
     public void Die()
     {
         gameObject.SetActive(false);
+
+        // 씬에 존재하는 GameManager 타입의 오브젝트를 찾아서 가져오기
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        // 가져온 GameManager 오브젝트의 EndGame() 메서드 실행
+        gameManager.EndGame();
     }
 }
